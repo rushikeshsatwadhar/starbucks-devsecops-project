0f78422fdfeb59142653a30269c2c1d85da3e05a
Rushikesh Satwadhar <85403690+rushikeshsatwadhar@users.noreply.github.com>
2026-08-09 18:43:58 +0530

    changing version

--- scripts/prometheus.sh
+++ scripts/prometheus.sh
@@ -2,7 +2,7 @@
 #this script belong to "CLOUDASEEM" YOUTUBE CHANNEL
 
 # Define Prometheus version
-PROMETHEUS_VERSION="2.51.2"
+PROMETHEUS_VERSION="3.13.2"
 
 # Update system and install necessary packages
 echo "Updating system and installing dependencies..."
